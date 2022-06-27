c951affa573836438e00e29d38ae95aa90c6a651
Cristian_Suarez <cristiandamsuarez@gmail.com>
2022-06-26 21:24:18 -0300

    Final

--- MVT_CristianSuarez/MTVCoder/MTVCoderApp/urls.py
+++ MVT_CristianSuarez/MTVCoder/MTVCoderApp/urls.py
@@ -1,9 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 
 from .views import *
 
 urlpatterns = [
     path('', inicio),
-    path('Familiares', familiares),
+    path('familiares', familiares),
 ]
